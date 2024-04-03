a24e73d08fef5bbc9552dff67c264c5154949e79
Matthew Denune <mattdenune@gmail.com>
2024-04-03 07:16:38 -0400

    displaying meal title from MealItem component

--- screens/CatagoriesScreen.js
+++ screens/CatagoriesScreen.js
@@ -1,6 +1,7 @@
 import { FlatList } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+
 import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
 
 
 function CategoriesScreen({ navigation }) {
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
+
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
-    const catID = route.params.categoryId;
+  const catID = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catID) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catID}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
