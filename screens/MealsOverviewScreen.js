6d9e861e59593a374cee7b895d617c21e3d51b30
Matthew Denune <mattdenune@gmail.com>
2024-04-04 07:00:38 -0400

    set navigation options dynamically

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,29 +1,38 @@
+import { useLayoutEffect } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
 
 import MealItem from "../components/MealItem";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catID = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catID) >= 0;
   });
 
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catID
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catID, navigation]);
+
   function renderMealItem(itemData) {
     const item = itemData.item;
-    
-    const mealItemProps = {
-        title: item.title,
-        imageUrl: item.imageUrl,
-        affordability: item.affordability,
-        complexity: item.complexity,
-        duration: item.duration,
-    }
 
-    return (
-      <MealItem {...mealItemProps} />
-    );
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
