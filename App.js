eb552be8504671bc4efd88e11b80caee0174b010
Matthew Denune <mattdenune@gmail.com>
2024-04-08 06:32:15 -0400

    added button to header of MealDetialScreen component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -38,7 +38,11 @@ export default function App() {
             //   };
             // }}
           />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{
+            headerRight: () => {
+              return <Button title="Tap me" />
+            }
+          }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
