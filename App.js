84cc2d5ece02c204b1f75627475cf7b2b3752592
Matthew Denune <mattdenune@gmail.com>
2024-04-11 06:52:45 -0400

    commented out Context API, rebuilding with redux-toolkit

--- App.js
+++ App.js
@@ -5,12 +5,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
+import { Provider } from 'react-redux';
+
 
 import CategoriesScreen from "./screens/CatagoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoritesScreen from "./screens/FavoritesScreen";
-import FavoritesContextProvider from "./store/context/favorites-context";
+// import FavoritesContextProvider from "./store/context/favorites-context";
+import { store } from "./store/redux/store";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -54,7 +57,8 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */} 
+      <Provider></Provider>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -83,7 +87,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+        </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   );
 }
